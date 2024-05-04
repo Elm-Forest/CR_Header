ff1025395b790e7a45d1ad35f820ae87a82357de
Elm Forest <timbrightland@gmail.com>
2024-05-05 06:24:48 +0800

    fix bugs

--- MemoryNet.py
+++ MemoryNet.py
@@ -1,7 +1,11 @@
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
 
 import memory
+from SPANet import conv3x3
+from rrdb import RRDB
 
 
 ##########################################################################
@@ -367,6 +371,155 @@ class MemoryNet(nn.Module):
         return [stage3_img + x_rgb, stage2_img, stage1_img]
 
 
+class MemoryNet_C(nn.Module):
+    def __init__(self, in_c=3, out_c=3, n_feat=40, scale_unetfeats=20, scale_orsnetfeats=16, num_cab=8, kernel_size=3,
+                 reduction=4, bias=False):
+        super(MemoryNet_C, self).__init__()
+        # self.memory = memory.MemModule()
+        self.memory = memory.MemModule(ptt_num=2, num_cls=10, part_num=5, fea_dim=in_c)
+        act = nn.PReLU()
+        self.shallow_feat1 = nn.Sequential(conv(in_c, n_feat, kernel_size, bias=bias),
+                                           CAB(n_feat, kernel_size, reduction, bias=bias, act=act))
+        self.shallow_feat2 = nn.Sequential(conv(in_c, n_feat, kernel_size, bias=bias),
+                                           CAB(n_feat, kernel_size, reduction, bias=bias, act=act))
+        self.shallow_feat3 = nn.Sequential(conv(in_c, n_feat, kernel_size, bias=bias),
+                                           CAB(n_feat, kernel_size, reduction, bias=bias, act=act))
+
+        # Cross Stage Feature Fusion (CSFF)
+        self.stage1_encoder = Encoder(n_feat, kernel_size, reduction, act, bias, scale_unetfeats, csff=False)
+        self.stage1_decoder = Decoder(n_feat, kernel_size, reduction, act, bias, scale_unetfeats)
+
+        self.stage2_encoder = Encoder(n_feat, kernel_size, reduction, act, bias, scale_unetfeats, csff=True)
+        self.stage2_decoder = Decoder(n_feat, kernel_size, reduction, act, bias, scale_unetfeats)
+
+        self.stage3_orsnet = ORSNet(n_feat, scale_orsnetfeats, kernel_size, reduction, act, bias, scale_unetfeats,
+                                    num_cab)
+
+        self.sam12 = SAM(n_feat, kernel_size=1, bias=bias, in_c=in_c)
+        self.sam23 = SAM(n_feat, kernel_size=1, bias=bias, in_c=in_c)
+
+        self.concat12 = conv(n_feat * 2, n_feat, kernel_size, bias=bias)
+        self.concat23 = conv(n_feat * 2, n_feat + scale_orsnetfeats, kernel_size, bias=bias)
+        self.tail1 = conv(in_c, out_c, kernel_size, bias=bias)
+        self.tail2 = conv(in_c, out_c, kernel_size, bias=bias)
+        self.tail3 = conv(n_feat + scale_orsnetfeats, out_c, kernel_size, bias=bias)
+
+    def forward(self, s2_img, s1_img):
+        x3_img = torch.cat((s2_img, s1_img), dim=1)
+        x_rgb = s2_img[:, 1:4, :, :]
+        H = x3_img.size(2)
+        W = x3_img.size(3)
+        ##通过memory模块使得变为三个分支
+        x1, x2, x3 = self.memory(x3_img)
+        # x3bot_img  = x3_img
+        # Multi-Patch Hierarchy: Split Image into four non-overlapping patches
+        # Two Patches for Stage 2
+        x2top_img = x2[:, :, 0:int(H / 2), :]
+        x2bot_img = x2[:, :, int(H / 2):H, :]
+
+        x3top_img = x3[:, :, 0:int(H / 2), :]
+        x3bot_img = x3[:, :, int(H / 2):H, :]
+
+        # Four Patches for Stage 1
+        x1ltop_img = x3top_img[:, :, :, 0:int(W / 2)]
+        x1rtop_img = x3top_img[:, :, :, int(W / 2):W]
+        x1lbot_img = x3bot_img[:, :, :, 0:int(W / 2)]
+        x1rbot_img = x3bot_img[:, :, :, int(W / 2):W]
+        ##-------------------------------------------
+        ##-------------- Stage 1---------------------
+        ##-------------------------------------------
+        ## Compute Shallow Features
+        x1ltop = self.shallow_feat1(x1ltop_img)
+        x1rtop = self.shallow_feat1(x1rtop_img)
+        x1lbot = self.shallow_feat1(x1lbot_img)
+        x1rbot = self.shallow_feat1(x1rbot_img)
+
+        ## Process features of all 4 patches with Encoder of Stage 1
+        feat1_ltop = self.stage1_encoder(x1ltop)
+        feat1_rtop = self.stage1_encoder(x1rtop)
+        feat1_lbot = self.stage1_encoder(x1lbot)
+        feat1_rbot = self.stage1_encoder(x1rbot)
+
+        ## Concat deep features
+        feat1_top = [torch.cat((k, v), 3) for k, v in zip(feat1_ltop, feat1_rtop)]
+        feat1_bot = [torch.cat((k, v), 3) for k, v in zip(feat1_lbot, feat1_rbot)]
+
+        ## Pass features through Decoder of Stage 1
+        res1_top = self.stage1_decoder(feat1_top)
+        res1_bot = self.stage1_decoder(feat1_bot)
+
+        ## Apply Supervised Attention Module (SAM)
+        x2top_samfeats, stage1_img_top = self.sam12(res1_top[0], x2top_img)
+        x2bot_samfeats, stage1_img_bot = self.sam12(res1_bot[0], x2bot_img)
+
+        ## Output image at Stage 1
+        stage1_img = torch.cat([stage1_img_top, stage1_img_bot], 2)
+        stage1_img = self.tail1(stage1_img)
+        ##-------------------------------------------
+        ##-------------- Stage 2---------------------
+        ##-------------------------------------------
+        ## Compute Shallow Features
+        x2top = self.shallow_feat2(x2top_img)
+        x2bot = self.shallow_feat2(x2bot_img)
+
+        ## Concatenate SAM features of Stage 1 with shallow features of Stage 2
+        x2top_cat = self.concat12(torch.cat([x2top, x2top_samfeats], 1))
+        x2bot_cat = self.concat12(torch.cat([x2bot, x2bot_samfeats], 1))
+
+        ## Process features of both patches with Encoder of Stage 2
+        feat2_top = self.stage2_encoder(x2top_cat, feat1_top, res1_top)
+        feat2_bot = self.stage2_encoder(x2bot_cat, feat1_bot, res1_bot)
+
+        ## Concat deep features
+        feat2 = [torch.cat((k, v), 2) for k, v in zip(feat2_top, feat2_bot)]
+
+        ## Pass features through Decoder of Stage 2
+        res2 = self.stage2_decoder(feat2)
+
+        ## Apply SAM
+        x3_samfeats, stage2_img = self.sam23(res2[0], x1)
+        stage2_img = self.tail2(stage2_img)
+        ##-------------------------------------------
+        ##-------------- Stage 3---------------------
+        ##-------------------------------------------
+        ## Compute Shallow Features
+        x3 = self.shallow_feat3(x1)
+
+        ## Concatenate SAM features of Stage 2 with shallow features of Stage 3
+        x3_cat = self.concat23(torch.cat([x3, x3_samfeats], 1))
+
+        x3_cat = self.stage3_orsnet(x3_cat, feat2, res2)
+        # x3_cat = self.relu(self.tail3(x3_cat) + x3_img)
+        stage3_img = self.tail3(x3_cat)
+        return [stage3_img + x_rgb, stage2_img, stage1_img]
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, use_norm=True):
+        super(Bottleneck, self).__init__()
+        m = OrderedDict()
+        m['conv1'] = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        if use_norm:
+            m['bc1'] = nn.BatchNorm2d(out_channels)
+        m['relu1'] = nn.ReLU(True)
+        m['conv2'] = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=2, bias=False, dilation=2)
+        if use_norm:
+            m['bc2'] = nn.BatchNorm2d(out_channels)
+        m['relu2'] = nn.ReLU(True)
+        m['conv3'] = nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=False)
+        self.group1 = nn.Sequential(m)
+        self.relu = nn.ReLU(True)
+        m['relu2'] = nn.ReLU(True)
+        m['conv3'] = nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=False)
+        self.group1 = nn.Sequential(m)
+        self.relu = nn.ReLU(True)
+
+    def forward(self, x):
+        out = self.group1(x)
+        out = self.relu(out + x)
+        return out
+
+
 class MemoryNet2(nn.Module):
     def __init__(self, in_c=3, in_s1=2, out_c=3, n_feat=40, scale_unetfeats=20, scale_orsnetfeats=16, num_cab=8,
                  kernel_size=3,
@@ -407,7 +560,26 @@ class MemoryNet2(nn.Module):
         self.tail2 = conv(in_c, out_c, kernel_size, bias=bias)
         self.tail3 = conv(n_feat + scale_orsnetfeats, in_c, kernel_size=3, bias=True)
         self.relu = nn.ReLU(True)
-        self.tail = conv(in_c, out_c, kernel_size, bias=bias)
+        self.res_in = nn.Sequential(
+            conv3x3(in_c, n_feat),
+            nn.ReLU(True),
+        )
+        self.rrdb_blocks = nn.Sequential(*[RRDB(n_feat) for _ in range(4)])
+        # self.cbam1 = CBAM(n_feat, no_spatial=True)
+        # self.cbam2 = CBAM(n_feat, no_spatial=True)
+        # self.res_blocks1 = nn.Sequential(
+        #     Bottleneck(n_feat, n_feat),
+        #     Bottleneck(n_feat, n_feat),
+        # )
+        # self.res_blocks2 = nn.Sequential(
+        #     Bottleneck(n_feat, n_feat),
+        #     Bottleneck(n_feat, n_feat),
+        # )
+        # self.res_blocks3 = nn.Sequential(
+        #     Bottleneck(n_feat, n_feat),
+        #     Bottleneck(n_feat, n_feat),
+        # )
+        self.tail = conv(n_feat, out_c, kernel_size, bias=bias)
 
     def forward(self, s2_img, s1_img):
         H = s2_img.size(2)
@@ -505,12 +677,34 @@ class MemoryNet2(nn.Module):
         x3_cat = self.concat23(torch.cat([x3, x3_samfeats], 1))
         x3_cat = self.stage3_orsnet(x3_cat, feat2, res2)
         x3_cat = self.relu(self.tail3(x3_cat) + s2_img)
-        stage3_img = self.tail(x3_cat)
+        outs = self.res_in(x3_cat)
+        outs = self.rrdb_blocks(outs)
+        # out = nn.functional.relu(self.cbam1(out) + out)
+        # out = self.res_blocks1(out)
+        # out = nn.functional.relu(self.cbam2(out) + out)
+        # out = self.res_blocks2(out)
+        # out = self.res_blocks3(out)
+        stage3_img = self.tail(outs)
         return [stage3_img, stage2_img, stage1_img]
 
 
 if __name__ == '__main__':
+    cloudy = torch.zeros((1, 13, 256, 256))
     s21 = torch.zeros((1, 13, 256, 256))
     s22 = torch.zeros((1, 13, 256, 256))
     s1 = torch.zeros((1, 2, 256, 256))
-    meta_learner = MemoryNet2(in_c=26 + 2)
+    # meta_learner = MemoryNet2(in_c=39, in_s1=2, n_feat=64, scale_unetfeats=32)
+    meta_learner = MemoryNet_C(in_c=13 + 2, n_feat=64, scale_unetfeats=32)
+    # for name, param in meta_learner.named_parameters():
+    #     # 默认情况下冻结所有参数
+    #     param.requires_grad = False
+    #
+    #     # 检查参数名前缀，如果匹配特定模块，则解冻
+    #     if name.startswith('res_in') or name.startswith('cbam1') or \
+    #             name.startswith('cbam2') or name.startswith('res_blocks1') or \
+    #             name.startswith('res_blocks2') or name.startswith('res_blocks3') or \
+    #             name.startswith('tail'):
+    #         param.requires_grad = True
+    outp = meta_learner(cloudy, s1)
+    torch.save(meta_learner.state_dict(), './model_test/mem.pth')
+    # outp = meta_learner(torch.cat((cloudy, s21, s22), dim=1), s1)
